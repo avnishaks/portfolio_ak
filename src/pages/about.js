f18de1579a32fdb42a5599151618d574bc07b5f6
Avnish Kumar <88883963+acko-avnish@users.noreply.github.com>
2024-05-11 12:33:57 +0530

    number update

--- src/pages/about.js
+++ src/pages/about.js
@@ -99,7 +99,7 @@ export default function About(){
                                 <h2 className="text-xl font-medium capitalize text-dark dark:text-light/75 xl:text-center md:text-lg sm:text-base xs:text-sm">CodeChef</h2>
                             </div>
                             <div className="flex flex-col items-end justify-center xl:items-center">
-                                <span className="inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl"><AnimateNumber value={650}/>+</span>
+                                <span className="inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl"><AnimateNumber value={600}/>+</span>
                                 <h2 className="text-xl font-medium capitalize text-dark dark:text-light/75 xl:text-center md:text-lg sm:text-base xs:text-sm">LeetCode</h2>
                             </div>
                             <div className="flex flex-col items-end justify-center xl:items-center">
