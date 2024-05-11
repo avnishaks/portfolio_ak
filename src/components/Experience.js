f0841d2d494a2791b53e490ef781ab3711283938
Avnish Kumar <88883963+acko-avnish@users.noreply.github.com>
2024-05-11 12:25:02 +0530

    view in dark mode enable

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -8,8 +8,8 @@ const Details = ({ position, company, companyLink, time, address, work }) => {
     return <li ref={ref} className='my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col justify-between md:w-[80%]'>
         <LiIcon reference={ref} />
         <motion.div initial={{ y: 50 }} whileInView={{ y: 0 }} transition={{ duration: 0.5, type: "spring" }}>
-            <h3 className="capitalize font-bold text-2xl sm:text-xl xs:text-lg">{position}&nbsp;<a href={companyLink} target="_blank" className="capitalize text-primary dark:text-primaryDark">@{company}</a></h3>
-            <span className="capitalize font-medium text-dark/75 xs:text-sm">{time} | {address}</span>
+            <h3 className="capitalize font-bold text-2xl sm:text-xl xs:text-lg dark:text-light">{position}&nbsp;<a href={companyLink} target="_blank" className="capitalize text-primary dark:text-primaryDark">@{company}</a></h3>
+            <span className="capitalize font-medium text-dark/75 xs:text-sm dark:text-light">{time} | {address}</span>
             <p className="font-medium w-full md:text-sm">{work}</p>
         </motion.div>
     </li>
